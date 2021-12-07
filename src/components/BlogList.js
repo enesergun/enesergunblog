5b7a1f5694748042d84d55d492f383e5b3825a8d
enesergun1515@gmail.com <63712936+enesergun@users.noreply.github.com>
2021-12-07 20:07:02 +0300

    blog posts returned

--- src/components/BlogList.js
+++ src/components/BlogList.js
@@ -1,11 +1,18 @@
 import React, { useState, useEffect } from 'react'; 
-import Article from './Article'
+import MarkDown from 'markdown-to-jsx';
+import Article from './Article' 
+import POSTS from '../posts/posts'
 
 
 function BlogList() {
+        
     return (
         <div>
-            tüm yazılar
+            {
+                POSTS.map((post)=> (
+                    <div>{post.title}</div>
+                ))
+            }
         </div>
     )
 }
---
+++ src/posts/posts.js
@@ -0,0 +1,19 @@
+const POSTS = [
+    {
+        id:0,
+        title:"deneme1",
+        file:"deneme.md"
+    },
+    {
+        id:1,
+        title:"deneme2",
+        file:"deneme2.md"
+    },
+    {
+        id:2,
+        title:"deneme3",
+        file:"deneme3.md"
+    },
+]
+
+export default POSTS;
