01fd611a6928f436abe1b2a7fb68b6104753aa4b
enesergun1515@gmail.com <63712936+enesergun@users.noreply.github.com>
2021-12-08 11:23:33 +0300

    markdown return article component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import './App.css';
 import BlogList from './components/BlogList'
-import { BrowserRouter as Router, Routes, Route, Outlet, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import POSTS from './posts/posts'
 import Article from './components/Article'
 
@@ -14,19 +14,14 @@ function App() {
       <div className="App">
         {
           POSTS.map((post, i) => (
-            
             <div key={i}> 
-
               <Link onClick={() => setId(post.id )} to={`/${post.id}`}>{post.title}</Link>
-              
             </div>
           ))
         }
-
         <Routes>
           <Route path="/" element={<BlogList />} />
           <Route path={`/${id}`} element={<Article postID={id} />} />
-
         </Routes>
       </div>
     
--- src/components/Article.js
+++ src/components/Article.js
@@ -1,13 +1,29 @@
 import React, { useState, useEffect } from 'react'; 
 import MarkDown from 'markdown-to-jsx';
+import POSTS from '../posts/posts'
 
 function Article(props) {
-    return (
-        <div>
-            asdas
-            {props.postID}
-        </div>
-    )
+    const file_name = `${POSTS[props.postID].file}`;
+    
+	const [post, setPost] = useState('');
+
+	useEffect(() => {
+		import(`../posts/POSTS/${file_name}`)
+			.then(res => {
+				fetch(res.default)
+					.then(res => res.text())
+					.then(res => setPost(res));
+			})
+			.catch(err => console.log(err));
+	});
+ 
+	return (
+		<div>
+			<MarkDown>
+				{post}
+			</MarkDown>
+		</div>
+	);
 }
 
 export default Article
